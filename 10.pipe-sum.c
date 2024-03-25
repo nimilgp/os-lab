3f44d32e78bd93767aadd06715d89f0c3366c80e
nimil <nimilgeorgepradeepngp@gmail.com>
2024-03-25 08:11:39 +0530

    can send data through pipe parent->child

--- 10.pipe-sum.c
+++ 10.pipe-sum.c
@@ -8,6 +8,7 @@ main(void)
 {
 	int pipefd_parentTOchild[2];
 	int pipefd_childTOparent[2];
+	int length;
 	pid_t cpid;
 
 	if(pipe(pipefd_parentTOchild)==-1 || pipe(pipefd_childTOparent)==-1) {
@@ -20,13 +21,22 @@ main(void)
 		case -1:
 			printf("error in forking\n");
 			return -1;
+
 		case 0: //child proc
-			close(pipefd_parentTOchild[1]) //closes write end of parent->child
-			close(pipefd_childToParent[0]) //closes read end of child->parent
+			close(pipefd_parentTOchild[1]); //closes write end of parent->child
+			close(pipefd_childTOparent[0]); //closes read end of child->parent
+			
+			read(pipefd_parentTOchild[0], &length, sizeof(int));
+			printf("number:%d\n",length);
 			return 0;
+	
 		default: //parent proc
-			close(pipefd_parentTOchild[0]) //closes read end of parent->child
-			close(pipefd_childToParent[1]) //closes write end of child->parent
+			close(pipefd_parentTOchild[0]); //closes read end of parent->child
+			close(pipefd_childTOparent[1]); //closes write end of child->parent
+
+			printf("enter number of elements:");
+			scanf("%d", &length);
+			write(pipefd_parentTOchild[1], &length, sizeof(int));
 			return 0;
 	}
 }
